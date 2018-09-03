ffd1a41e7a2a0d03454ec89b10290160098923ae
s102122 <s102122@ap.be>
2018-09-03 16:31:22 +0200

    first run
    first run

--- server.js
+++ server.js
@@ -14,3 +14,33 @@ app.use(express.static('public'));
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
+
+app.get('/answer', (req, res) => {	
+	var antwoorden = new Array()
+	
+	antwoorden[0] = 'It is certain.'
+	antwoorden[1] = 'It is decidedly so.'
+	antwoorden[2] = 'Without a doubt.'
+	antwoorden[3] = 'Yes- definitely.'
+	antwoorden[4] = 'You may rely on it.'
+	antwoorden[5] = 'As I see it, yes.'
+	antwoorden[6] = 'Most Likely.'
+	antwoorden[7] = 'Outlook good.'
+	antwoorden[8] = 'Yes.'
+	antwoorden[9] = 'Signs point to yes.'
+	antwoorden[10] = 'Reply hazy, try again.'
+	antwoorden[11] = 'Ask again later.'
+	antwoorden[12] = 'Better not tell you now.'
+	antwoorden[13] = 'Cannot predict now.'
+	antwoorden[14] = 'Concentrate and ask again.'
+	antwoorden[15] = 'Do not count on it.'
+	antwoorden[16] = 'My reply is no.'
+	antwoorden[17] = 'My sources say no.'
+	antwoorden[18] = 'Outlook not so good.'
+	antwoorden[19] = 'Very doubtful.'	
+	
+	result = antwoorden[Math.floor(Math.random() * 20)]
+	
+	
+	res.render('index.ejs', {antwoord: result})
+})
